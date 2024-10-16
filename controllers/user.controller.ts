f31be8716555dacb29002440cca945e2f8d0a54b
Andres Arcila <andresarcilaa2@me.com>
2024-10-16 10:00:08 -0500

    new methods

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -7,3 +7,31 @@ export async function getUsers(): Promise<Response> {
     headers: { "Content-Type": "application/json" },
   });
 }
+
+export async function createUser(request: Request): Promise<Response> {
+  const user = await request.json();
+  const result = await userService.createUser(user);
+  return new Response(JSON.stringify(result), {
+    status: result.error ? 400 : 201,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export async function updateUser(request: Request): Promise<Response> {
+  const { id } = request.params; // Asumiendo que el ID se pasa como parámetro
+  const user = await request.json();
+  const result = await userService.updateUser(Number(id), user);
+  return new Response(JSON.stringify(result), {
+    status: result.error ? 400 : 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export async function deleteUser(request: Request): Promise<Response> {
+  const { id } = request.params; // Asumiendo que el ID se pasa como parámetro
+  const result = await userService.deleteUser(Number(id));
+  return new Response(JSON.stringify(result), {
+    status: result.error ? 400 : 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,11 +1,16 @@
-import { getUsers } from "../controllers/user.controller.ts";
+import { createUser, deleteUser, getUsers, updateUser } from "../controllers/user.controller.ts";
 
 export default async function userRoutes(request: Request): Promise<Response> {
   const url = new URL(request.url);
   const { pathname } = url;
-
   if (request.method === "GET" && pathname === "/api/users") {
     return await getUsers();
+  } else if (request.method === "POST" && pathname === "/api/users") {
+    return await createUser(request);
+  } else if (request.method === "PUT" && pathname.startsWith("/api/users/")) {
+    return await updateUser(request);
+  } else if (request.method === "DELETE" && pathname.startsWith("/api/users/")) {
+    return await deleteUser(request);
   }
 
   return new Response("Not Found", { status: 404 });
