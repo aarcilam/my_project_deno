e2dc23160ad50597dc80d4161e89378e57649cfd
Andres Arcila <andresarcilaa2@me.com>
2024-10-16 09:57:18 -0500

    interface user

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,16 +1,48 @@
 import db from "../db/db.ts";
+import {User} from "../interfaces/User.interface.ts"
 
 export function getUsers() {
   let users;
 
   try {
-    // Ejecutar la consulta y recorrer los resultados
     const stmt = db.prepare("SELECT * FROM users");
     users = stmt.all();
-    console.log(users);
   } catch (error) {
     console.error("Error fetching users:", error);
   }
 
   return users;
+}
+
+export function createUser(user:User) {
+  try {
+    const stmt = db.prepare("INSERT INTO users (name, email) VALUES (?, ?)");
+    stmt.run(user.name, user.email);
+    return { message: "User created successfully" };
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return { error: "Error creating user" };
+  }
+}
+
+export function updateUser(id:number, user:User) {
+  try {
+    const stmt = db.prepare("UPDATE users SET name = ?, email = ? WHERE id = ?");
+    stmt.run(user.name, user.email, id);
+    return { message: "User updated successfully" };
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return { error: "Error updating user" };
+  }
+}
+
+export function deleteUser(id:number) {
+  try {
+    const stmt = db.prepare("DELETE FROM users WHERE id = ?");
+    stmt.run(id);
+    return { message: "User deleted successfully" };
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    return { error: "Error deleting user" };
+  }
 }
